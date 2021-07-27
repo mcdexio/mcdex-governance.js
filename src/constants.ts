2490510c4c6f3932e0c9e7b8c610e1a2503a7b9c
mcarlo-mzc <mazhichao@mcarlo.com>
2021-07-27 11:21:17 +0800

    add optimism dao contract address

--- src/constants.ts
+++ src/constants.ts
@@ -11,7 +11,9 @@ export const CHAIN_ID_TO_DAO_XMCB_ADDRESS: { [chainID: number]: string } = {
     // arb testnet
     421611: '0xee3f6Dc8168Fe907c24C50feCda14050147024F7',
     // arb one
-    42161: '0x1284c70F0ed8539F450584Ce937267F8C088B4cC'
+    42161: '0x1284c70F0ed8539F450584Ce937267F8C088B4cC',
+    // ovm testnet
+    69: '0x413448A5BEFF978ab820117E9cB83dA590d81959',
 }
 
 export const CHAIN_ID_TO_DAO_MINTER_ADDRESS: { [chainID: number]: string } = {
@@ -22,7 +24,9 @@ export const CHAIN_ID_TO_DAO_MINTER_ADDRESS: { [chainID: number]: string } = {
     // arb testnet
     421611: '0x2C14b56bA185709604265beedaca3E177699cb77',
     // arb one
-    42161: '0x7230D622D067D9C30154a750dBd29C035bA7605a'
+    42161: '0x7230D622D067D9C30154a750dBd29C035bA7605a',
+    // ovm testnet
+    69: '0x353a4Cb4e37f8AB4e4274f8bcE65DE902AF04Fc0',
 }
 
 export const CHAIN_ID_TO_DAO_MINING_ADDRESS: { [chainID: number]: string } = {
@@ -33,7 +37,9 @@ export const CHAIN_ID_TO_DAO_MINING_ADDRESS: { [chainID: number]: string } = {
     // arb testnet
     421611: '0xB3869A26159066A8AD9789882b22f7A6EA6cA76a',
     // arb one
-    42161: '0x694baa24d46530E46BCd39b1F07943a2BDDb01e6'
+    42161: '0x694baa24d46530E46BCd39b1F07943a2BDDb01e6',
+    // ovm testnet
+    69: '0xA68B598905D557418e9c0d252a3c160645527785',
 }
 
 export const CHAIN_ID_TO_DAO_VALUE_CAPTURE_ADDRESS: { [chainID: number]: string } = {
@@ -44,7 +50,9 @@ export const CHAIN_ID_TO_DAO_VALUE_CAPTURE_ADDRESS: { [chainID: number]: string 
     // arb testnet
     421611: '0xB25a6a1De2C17bfeEb721eaeeC470fbA9E0E0aFB',
     // arb one
-    42161: '0xa04197E5F7971E7AEf78Cf5Ad2bC65aaC1a967Aa'
+    42161: '0xa04197E5F7971E7AEf78Cf5Ad2bC65aaC1a967Aa',
+    // ovm testnet
+    69: '0xF7292A449bd1B7E3c9d701742f52fA92305a99FA',
 }
 
 
@@ -56,7 +64,9 @@ export const CHAIN_ID_TO_DAO_VAULT_ADDRESS: { [chainID: number]: string } = {
     // arb testnet
     421611: '0xcC52CE02158Bf55779E8434B82b5940d05994d3C',
     // arb one
-    42161: '0xD78ba1D99dbBC4ebA3B206c9C67a08879b6eC79B'
+    42161: '0xD78ba1D99dbBC4ebA3B206c9C67a08879b6eC79B',
+    // ovm testnet
+    69: '0xD36eB928cE31CDaBBe16B6d84Cb5Fe173bE44ecd',
 }
 
 export const CHAIN_ID_TO_DAO_GOVERNOR_ADDRESS: { [chainID: number]: string } = {
@@ -67,7 +77,9 @@ export const CHAIN_ID_TO_DAO_GOVERNOR_ADDRESS: { [chainID: number]: string } = {
     // arb testnet
     421611: '0xaf977F17EB9Fc0e1f42266b899f725E102503770',
     // arb one
-    42161: '0x8597eB9E005f39f8f70A17aeA914B20450ABfE60'
+    42161: '0x8597eB9E005f39f8f70A17aeA914B20450ABfE60',
+    // ovm testnet
+    69: '0xa321e10Cf4867dD430298db5B880b5279b80C032',
 }
 
 export const CHAIN_ID_TO_DAO_TIMELOCK_ADDRESS: { [chainID: number]: string } = {
@@ -78,5 +90,7 @@ export const CHAIN_ID_TO_DAO_TIMELOCK_ADDRESS: { [chainID: number]: string } = {
     // arb testnet
     421611: '0x6Ab6A2bcE38287B4C068d5CC3f65F19FE5a2c519',
     // arb one
-    42161: '0xC1D816Ad4Ee531994D940C6C3ECEdb948Dc2735e'
+    42161: '0xC1D816Ad4Ee531994D940C6C3ECEdb948Dc2735e',
+    // ovm testnet
+    69: '0x571fA159F53b399a1A2363a5f73acD2B4d09160B',
 }
